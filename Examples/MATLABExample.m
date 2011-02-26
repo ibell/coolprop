880444c0af37c44ca04272da51a9014d4ba45e78
ibell <ibell@5c531a33-090d-464c-98f4-7bffe1c54a66>
2011-02-26 01:20:48 +0000

    CoolProp Matlab example typo fixed

--- Examples/MATLABExample.m
+++ Examples/MATLABExample.m
@@ -6,6 +6,6 @@ disp 'The critical temperature of R410A is:'
 res=calllib('CoolPropDLL','Tcrit_dll','R410A');
 disp(res)
 disp 'The saturated vapor enthalpy of Propane at 275 K is:'
-res=calllib('CoolPropDLL','Props_dll',int8('H'),int8('T'),275.0,int8('Q'),1.0,'R290')
+res=calllib('CoolPropDLL','Props_dll',int8('H'),int8('T'),275.0,int8('Q'),1.0,'R290');
 disp(res)
 unloadlibrary('CoolPropDLL')
