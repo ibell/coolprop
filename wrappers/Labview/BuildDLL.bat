f0b193c06f474737fccc0c7ff2d2b5ca8d66ee32
ibell <ibell@5c531a33-090d-464c-98f4-7bffe1c54a66>
2013-02-21 20:59:05 +0000

    Updated Labview build script to target real-time applications

--- wrappers/Labview/BuildDLL.bat
+++ wrappers/Labview/BuildDLL.bat
@@ -1,13 +1,16 @@
 REM ******** set the variables ************
-call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
-
+call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
 REM ******* compile all the sources ***************
-cl /c /I../../CoolProp /EHsc /MT /DCOOLPROP_LIB /DCONVENTION=__cdecl ../../CoolProp/*.cpp
-cl /c /I../../CoolProp /EHsc /MT /DCOOLPROP_LIB /DCONVENTION=__cdecl ../../CoolProp/purefluids/*.cpp
-cl /c /I../../CoolProp /EHsc /MT /DCOOLPROP_LIB /DCONVENTION=__cdecl ../../CoolProp/pseudopurefluids/*.cpp
 
-link /DLL CoolProp.obj *.obj /OUT:CoolProp.dll
+cl /O2 /Oi /GL /I "..\..\CoolProp" /D "WIN32" /D "NDEBUG" /D "_WINDOWS" /D "_USRDLL" /D "LABVIEW_RT_EXPORTS" /D "COOLPROP_LIB" /D "CONVENTION=__cdecl" /D "_WINDLL" /FD /EHsc /MT /Gy /W3 /c /Zi /TP ..\..\CoolProp\*.cpp
+cl /O2 /Oi /GL /I "..\..\CoolProp" /D "WIN32" /D "NDEBUG" /D "_WINDOWS" /D "_USRDLL" /D "LABVIEW_RT_EXPORTS" /D "COOLPROP_LIB" /D "CONVENTION=__cdecl" /D "_WINDLL" /FD /EHsc /MT /Gy /W3 /c /Zi /TP ..\..\CoolProp\purefluids\*.cpp
+cl /O2 /Oi /GL /I "..\..\CoolProp" /D "WIN32" /D "NDEBUG" /D "_WINDOWS" /D "_USRDLL" /D "LABVIEW_RT_EXPORTS" /D "COOLPROP_LIB" /D "CONVENTION=__cdecl" /D "_WINDLL" /FD /EHsc /MT /Gy /W3 /c /Zi /TP ..\..\CoolProp\pseudopurefluids\*.cpp
+
+link /OUT:"CoolProp.dll" /INCREMENTAL:NO /NOLOGO /DLL /MANIFEST:NO /DEBUG /SUBSYSTEM:WINDOWS /OPT:REF /OPT:ICF /LTCG /DYNAMICBASE /NXCOMPAT /MACHINE:X86 /ERRORREPORT:PROMPT kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib *.obj
+
 dumpbin /EXPORTS CoolProp.dll > exports.txt
 erase *.obj
+erase *.pdb
+erase *.idb
 erase *.lib
 erase *.exp
