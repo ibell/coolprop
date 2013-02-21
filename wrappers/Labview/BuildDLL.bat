7a9c35ea45337ead2128bc7a848093061bb537b0
ibell <ibell@5c531a33-090d-464c-98f4-7bffe1c54a66>
2013-02-21 19:53:03 +0000

    Updated DLL build script for Labview to use static runtime

--- wrappers/Labview/BuildDLL.bat
+++ wrappers/Labview/BuildDLL.bat
@@ -2,9 +2,9 @@ REM ******** set the variables ************
 call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 
 REM ******* compile all the sources ***************
-cl /c /I../../CoolProp /EHsc /DCOOLPROP_LIB /DCONVENTION=__cdecl ../../CoolProp/*.cpp
-cl /c /I../../CoolProp /EHsc /DCOOLPROP_LIB /DCONVENTION=__cdecl ../../CoolProp/purefluids/*.cpp
-cl /c /I../../CoolProp /EHsc /DCOOLPROP_LIB /DCONVENTION=__cdecl ../../CoolProp/pseudopurefluids/*.cpp
+cl /c /I../../CoolProp /EHsc /MT /DCOOLPROP_LIB /DCONVENTION=__cdecl ../../CoolProp/*.cpp
+cl /c /I../../CoolProp /EHsc /MT /DCOOLPROP_LIB /DCONVENTION=__cdecl ../../CoolProp/purefluids/*.cpp
+cl /c /I../../CoolProp /EHsc /MT /DCOOLPROP_LIB /DCONVENTION=__cdecl ../../CoolProp/pseudopurefluids/*.cpp
 
 link /DLL CoolProp.obj *.obj /OUT:CoolProp.dll
 dumpbin /EXPORTS CoolProp.dll > exports.txt
