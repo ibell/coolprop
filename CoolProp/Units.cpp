c077f9838b5d2ff68cf80c2d57196eb4bdc3295d
Ian Bell <ian.h.bell@gmail.com>
2013-08-29 16:50:08 +0200

    Unit conversion for Pressure does not use PressureUnit class (too slow)
    Signed-off-by: Ian Bell <ian.h.bell@gmail.com>

--- CoolProp/Units.cpp
+++ CoolProp/Units.cpp
@@ -7,15 +7,13 @@ double convert_from_unit_system_to_SI(long iInput, double value, int old_system)
 	switch (iInput)
 	{
 	case iP:
-		PU = PressureUnit();
-		PU.set_in_unit_system(value, old_system);
-		return PU.Pa;
 	case iC:
 	case iC0:
 	case iS:
 	case iO:
 	case iH:
 	case iU:
+	case iL:
 		switch (old_system)
 		{
 		case UNIT_SYSTEM_KSI:
@@ -35,19 +33,21 @@ double convert_from_unit_system_to_SI(long iInput, double value, int old_system)
 double convert_from_SI_to_unit_system(long iInput, double value, int new_system)
 {
 	PressureUnit PU;
+	if (new_system == UNIT_SYSTEM_SI)
+	{
+		return value;
+	}
 
 	switch (iInput)
 	{
 	case iP:
-		PU = PressureUnit();
-		PU.set_SI(value);
-		return PU.get_in_unit_system(new_system);
 	case iC:
 	case iC0:
 	case iS:
 	case iO:
 	case iH:
 	case iU:
+	case iL:
 		switch (new_system)
 		{
 		case UNIT_SYSTEM_KSI:
