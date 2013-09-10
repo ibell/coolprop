170461313c459d2159e0d421660d49f2a15c8498
Ian Bell <ian.h.bell@gmail.com>
2013-09-10 18:39:18 +0100

    Fixed all the _err_string warnings calls in python wrappers
    Signed-off-by: Ian Bell <ian.h.bell@gmail.com>

--- wrappers/Python/CoolProp/CoolProp.pyx
+++ wrappers/Python/CoolProp/CoolProp.pyx
@@ -115,7 +115,7 @@ cpdef double IProps(long iOutput, long iInput1, double Input1, long iInput2, dou
     cdef double val = _IProps(iOutput, iInput1, Input1, iInput2, Input2, iFluid)
     
     if math.isinf(val) or math.isnan(val):
-        err_string = _get_errstring()
+        err_string = _get_global_param_string('errstring')
         if not len(err_string) == 0:
             raise ValueError("{err:s} :: inputs were :{iin1:d},{in1:g},{iin2:d},{in2:g},{iFluid:d}".format(err= err_string,iin1=iInput1,in1=Input1,iin2=iInput2,in2=Input2,iFluid = iFluid))
         else:
@@ -892,7 +892,7 @@ cdef class State:
             if abs(T)<1e90:
                 self.T_=T
             else:
-                errstr = _get_errstring()
+                errstr = _get_global_param_string('errstring')
                 raise ValueError(errstr)
             self.rho_ = _Props('D','P',p,'H',h,self.Fluid)
             
@@ -921,7 +921,7 @@ cdef class State:
         if abs(p)<1e90:
             self.p_=p
         else:
-            errstr = _get_errstring()
+            errstr = _get_global_param_string('errstring')
             raise ValueError(errstr)
         
     cpdef update(self, dict params, double xL=-1.0):
@@ -972,7 +972,7 @@ cdef class State:
                 if abs(rho) < 1e90:
                     self.rho_=rho
                 else:
-                    errstr = _get_errstring()
+                    errstr = _get_global_param_string('errstring')
                     raise ValueError(errstr)
             elif 'D' in params:
                 self.rho_=params['D']
@@ -981,7 +981,7 @@ cdef class State:
                 if abs(p)<1e90:
                     self.p_=p
                 else:
-                    errstr = _get_errstring()
+                    errstr = _get_global_param_string('errstring')
                     raise ValueError(errstr+str(params))
             elif 'Q' in params:
                 p = _Props('P','T',self.T_,'Q',params['Q'],self.Fluid)
@@ -990,7 +990,7 @@ cdef class State:
                 if abs(self.rho_)<1e90:
                     pass
                 else:
-                    errstr = _get_errstring()
+                    errstr = _get_global_param_string('errstring')
                     raise ValueError(errstr+str(params))
             else:
                 raise KeyError("Dictionary must contain the key 'T' and one of 'P' or 'D'")
