3e48ebf75d955a04e604ddf2f0fa7a71429c4b1e
Logan Page <page.lg@gmail.com>
2013-09-13 00:46:22 +0200

    updated test script... need to move this to tests/test_plots.py file instead

--- wrappers/Python/CoolProp/Plots/Tests.py
+++ wrappers/Python/CoolProp/Plots/Tests.py
@@ -9,10 +9,13 @@ from Plots import Graph #TODO: Change to absolute import
 
 
 def main():
-    fluid_ref = 'R245fa'
-    for graph_type in ['Ts', 'Ps']: #['pt', 'ph', 'ps', 'ts', 'pt', 'prho', 'trho']:
+    fluid_ref = 'n-Pentane'
+    for graph_type in ['Ts']: #['pt', 'ph', 'ps', 'ts', 'pt', 'prho', 'trho']:
         graph = Graph(fluid_ref, graph_type)
+        graph.set_axis_limits([-0.5, 1.5, 300, 530])
         graph.draw_isolines('Q', [0.1, 0.9])
+        graph.draw_isolines('P', [100, 2000])
+        graph.draw_isolines('D', [2, 600])
         graph.show()
 
 if __name__ == "__main__":
