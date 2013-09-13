b342adecdbc296817cd25a53b19d0add08c2d6a9
Ian Bell <ian.h.bell@gmail.com>
2013-09-13 19:36:49 +0200

    CPState undoing the iR
    Signed-off-by: Ian Bell <ian.h.bell@gmail.com>

--- CoolProp/CPState.cpp
+++ CoolProp/CPState.cpp
@@ -998,8 +998,6 @@ double CoolPropStateClass::keyed_output(long iOutput)
 			return s();
 		case iU:
 			return h()-p()/_rho;
-		case iR:
-			return pFluid->R();
 
 		case iPhase:
 			return phase();
--- CoolProp/gitrevision.h
+++ CoolProp/gitrevision.h
@@ -1,1 +1,1 @@
-std::string gitrevision = "0aac0f8e2ddb69946faf71eb49e96ecb9f043198";
+std::string gitrevision = "8b89c0d9d22a44c50a988508ae806058ac7ca3e9";
