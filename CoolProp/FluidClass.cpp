c0c4f3acf7c83df7766cf924c067b7eea27da01f
Martin Ryhl Kærn <pmak@mek.dtu.dk>
2013-09-24 14:47:08 +0200

    Signed-off-by: Martin Ryhl Kærn <pmak@mek.dtu.dk>

--- CoolProp/FluidClass.cpp
+++ CoolProp/FluidClass.cpp
@@ -1,4 +1,4 @@
-#define _CRT_SECURE_NO_WARNINGS
+﻿#define _CRT_SECURE_NO_WARNINGS
 
 #include <stdlib.h>
 #include <string>
@@ -3183,7 +3183,7 @@ void Fluid::saturation_p(double p, bool UseLUT, double *TsatL, double *TsatV, do
 			SPGR.rhoL = rhoL;
 			SPGR.rhoV = rhoV;
 			try{
-				Tsat = Secant(&SPGR,Tsat,1e-4,1e-7,50,&errstr);
+				Tsat = Secant(&SPGR,Tsat,1e-2*Tsat,1e-8,50,&errstr);
 				if (errstr.size()>0 || !ValidNumber(Tsat)|| !ValidNumber(SPGR.rhoV)|| !ValidNumber(SPGR.rhoL))
 					throw SolutionError("Saturation calculation failed");
 				*rhoVout = SPGR.rhoV;
