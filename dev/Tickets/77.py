9146b981000236c592edd909a7d0acef717dc394
jowr <jowr@mek.dtu.dk>
2013-12-02 16:37:00 +0100

    trying to validate the derivatives

---
+++ dev/Tickets/77.py
@@ -0,0 +1,25 @@
+import matplotlib
+matplotlib.use('Qt4Agg')
+
+import numpy 
+import CoolProp.CoolProp as CP
+
+
+print "Testing the derivatives and store results:"
+
+keys = ["dpdT","dpdrho","Z","dZ_dDelta","dZ_dTau","B","dBdT","C","dCdT","phir","dphir_dTau","d2phir_dTau2","dphir_dDelta",
+  "d2phir_dDelta2","d2phir_dDelta_dTau","d3phir_dDelta2_dTau","phi0","dphi0_dTau","d2phi0_dTau2","dphi0_dDelta","d2phi0_dDelta2",
+  "IsothermalCompressibility"]
+
+fluid = "n-Pentane"
+
+T = numpy.array([ 30,100,150,210])+273.15
+D = numpy.array([710, 20, 20,210])
+
+for key in keys:
+    liquid    = CP.DerivTerms(key,T[0],D[0],fluid)
+    twophase  = CP.DerivTerms(key,T[1],D[1],fluid)
+    gaseous   = CP.DerivTerms(key,T[2],D[2],fluid)
+    supercrit = CP.DerivTerms(key,T[3],D[3],fluid)
+    print '{:<25}: {:>10.5f}; {:>10.5f}; {:>10.5f}; {:>10.5f}'.format(key,liquid,twophase,gaseous,supercrit)
+    
