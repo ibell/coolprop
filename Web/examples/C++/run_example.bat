2c7ac1777e9c757b666ef69d3176e28a877219af
Ian Bell <ian.h.bell@gmail.com>
2013-12-06 14:21:59 -0500

    Updated the files for the examples
    Signed-off-by: Ian Bell <ian.h.bell@gmail.com>

--- Web/examples/C++/run_example.bat
+++ Web/examples/C++/run_example.bat
@@ -5,7 +5,7 @@ call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
 
 REM ******* compile all the sources from CoolProp ***************
 cl /c /Ox /fp:fast /I../../../CoolProp /EHsc Example.cpp
-cl /c /Ox /fp:fast /I../../../CoolProp /EHsc ../../../CoolProp/*.cpp
+cl /c /Ox /MP3 /fp:fast /I../../../CoolProp /EHsc ../../../CoolProp/*.cpp
 
 link *.obj /OUT:Example.exe
 erase *.obj
--- Web/examples/CSharp/run_example.bat
+++ Web/examples/CSharp/run_example.bat
@@ -2,6 +2,7 @@ REM ******** set the variables ************
 call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
 call "C:\Program Files\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
 
+copy ..\..\..\wrappers\C#\Example.cs
 erase *_wrap.cpp
 
 swig.exe -csharp -dllimport "CoolProp" -c++ -outcurrentdir ../../../CoolProp/CoolProp.i
@@ -19,3 +20,9 @@ erase CoolProp.exp
 call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 csc *.cs /platform:x86
 call Example > Output.txt
+
+REM cleanup
+erase *.cs
+erase Example.exe
+erase CoolProp.dll
+copy ..\..\..\wrappers\C#\Example.cs
