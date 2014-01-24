0fa9ae87fc144fcdab011f55d6bdbd8e6e37e8e3
Ian Bell <ian.h.bell@gmail.com>
2014-01-24 12:36:34 +0100

    Restore EES library, added gcc -v to debug Travis segfaults
    Signed-off-by: Ian Bell <ian.h.bell@gmail.com>

--- wrappers/EES/BuildDLF.bat
+++ wrappers/EES/BuildDLF.bat
@@ -4,8 +4,6 @@ call "C:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 
 REM ******* compile all the sources from CoolProp***************
 cl /c /EHsc /I../../CoolProp maindll.cpp ../../CoolProp/CoolPropTools.cpp
-link /DLL mainSI.obj CoolPropTools.obj /OUT:COOLPROP_EES.dlf
+link /DLL maindll.obj CoolPropTools.obj /OUT:COOLPROP_EES.dlf
 
-erase COOLPROP_EES.exp
-erase COOLPROP_EES.lib
 erase *.obj
