fc19415db118a56e8ba0c5bb2865941ebab14141
jowr <jowr@mek.dtu.dk>
2014-02-24 15:55:14 +0100

    Fixed TTSE unit conversion error

--- CoolProp/CPState.cpp
+++ CoolProp/CPState.cpp
@@ -1641,8 +1641,7 @@ double CoolPropStateClassSI::isothermal_compressibility(void){
 			double drhodp__h = pFluid->TTSESinglePhase.evaluate_first_derivative(iD,iP,iH,_p,_logp,_h);
 
 			double rho = pFluid->TTSESinglePhase.evaluate(iD,_p,_logp,_h);
-			/// 1000 is needed to convert from kJ & kPa to J & Pa
-			return 1/rho*(drhodp__h-drhodh__p*dTdp__h/dTdh__p)/1000;
+			return 1/rho*(drhodp__h-drhodh__p*dTdp__h/dTdh__p);
 		}
 	}
 	else
