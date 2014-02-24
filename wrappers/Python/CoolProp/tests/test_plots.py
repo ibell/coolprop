46f1e8f0297557bee9b3e5dc5b18993c30ad0476
Ian Bell <ian.h.bell@gmail.com>
2014-02-24 14:25:41 +0100

    cleaned up tests - plots do not show
    Signed-off-by: Ian Bell <ian.h.bell@gmail.com>

--- wrappers/Python/CoolProp/tests/test_plots.py
+++ wrappers/Python/CoolProp/tests/test_plots.py
@@ -1,132 +1,93 @@
 import numpy as np
-import pylab
-#from CoolProp.Plots.Plots import getIsoLines, drawIsoLines, Ts
-
-#def test_Trho():
-#    for Fluid in CoolProp.__fluids__:
-#        for T in np.linspace(Props(Fluid,'Tmin')+1e-5,Props(Fluid,'Tcrit')+100,20):
-#            for rho in np.linspace(1e-10,Props(Fluid,'rhocrit')*3,20):
-#                yield check_rho,Fluid,T,rho
-#
-#def check_rho(Fluid,T,rho):
-#    p = Props('P','T',T,'D',rho,Fluid)
-#
-#def test_namedPlots():
-#    return True
-#
-#def test_functions():
-#
-#    Ref = 'n-Pentane'
-#
-#    # Version A, get lines and do the plotting here
-#    fig, ax = pylab.subplots(1, 1)
-#    # Set limits, be sure to use internal CoolProp Units!
-#    ax.set_xlim([-0.5,1.5])
-#    ax.set_ylim([300,530])
-#    lines = []
-#    lines.extend(getIsoLines(Ref, 'Ts', 'Q', [0.0, 0.6,  0.75, 0.775, 0.8, 1.0], axis=ax))
-#    lines.extend(getIsoLines(Ref, 'Ts', 'P', [100, 2000], num=5, axis=ax))
-#    lines.extend(getIsoLines(Ref, 'Ts', 'D', [2,    600], num=5, axis=ax))
-##    lines.extend(getIsoLines(Ref, 'Ts', 'H', 100,  300, 5, ax))
-#    for line in lines:
-#        ax.plot(line['x'],np.array(line['y'])-273.15,**line['opts'])
-#    # Adjust the T limits to Celsius
-#    ax.set_ylim([25,250])
-#
-#    # Version B, use built-in drawing functions and receiver line objects
-#    fig, ax = pylab.subplots(1, 1)
-#    ax = Ts(Ref)
-#    ax.set_xlim([-0.5,1.5])
-#    ax.set_ylim([300,530])
-#    quality    = drawIsoLines(Ref, 'Ts', 'Q', [0.3,  0.75, 0.775, 0.8], axis=ax)
-#    isobars    = drawIsoLines(Ref, 'Ts', 'P', [100, 2000]             , num=5, axis=ax)
-#    isochores  = drawIsoLines(Ref, 'Ts', 'D', [2,    600]             , num=7, axis=ax)
-##    isenthalps = drawIsoLines(Ref, 'Ts', 'H', 100,  300, 5, ax)
-#
-#
-#    #pylab.show()
-
+import matplotlib.pyplot as plt
 
 def test_back_compatibility():
     fluid_ref = 'R290'
 
     def Ts_plot_tests():
         from CoolProp.Plots import Ts
-        Ts(fluid_ref, show=True)
+        Ts(fluid_ref, show=False)
 
         from matplotlib import pyplot
         fig = pyplot.figure(2)
         ax = fig.gca()
-        Ts(fluid_ref, show=True, axis=ax)
-
-        Ts(fluid_ref, show=True, Tmin=200, Tmax=300)
+        Ts(fluid_ref, show=False, axis=ax)
+        plt.close()
+        Ts(fluid_ref, show=False, Tmin=200, Tmax=300)
+        plt.close()
 
     def Ph_plot_tests():
         from CoolProp.Plots import Ph
-        Ph(fluid_ref, show=True)
+        Ph(fluid_ref, show=False)
 
         from matplotlib import pyplot
         fig = pyplot.figure(2)
         ax = fig.gca()
-        Ph(fluid_ref, show=True, axis=ax)
-
-        Ph(fluid_ref, show=True, Tmin=200, Tmax=300)
+        Ph(fluid_ref, show=False, axis=ax)
+        plt.close()
+        Ph(fluid_ref, show=False, Tmin=200, Tmax=300)
+        plt.close()
 
     def PT_plot_tests():
         from CoolProp.Plots import PT
-        PT(fluid_ref, show=True)
+        PT(fluid_ref, show=False)
 
         from matplotlib import pyplot
         fig = pyplot.figure(2)
         ax = fig.gca()
-        PT(fluid_ref, show=True, axis=ax)
-
-        PT(fluid_ref, show=True, Tmin=200, Tmax=300)
+        PT(fluid_ref, show=False, axis=ax)
+        plt.close()
+        PT(fluid_ref, show=False, Tmin=200, Tmax=300)
+        plt.close()
 
     def Ps_plot_tests():
         from CoolProp.Plots import Ps
-        Ps(fluid_ref, show=True)
+        Ps(fluid_ref, show=False)
 
         from matplotlib import pyplot
         fig = pyplot.figure(2)
         ax = fig.gca()
-        Ps(fluid_ref, show=True, axis=ax)
-
-        Ps(fluid_ref, show=True, Tmin=200, Tmax=300)
+        Ps(fluid_ref, show=False, axis=ax)
+        plt.close()
+        Ps(fluid_ref, show=False, Tmin=200, Tmax=300)
+        plt.close()
 
     def Prho_plot_tests():
         from CoolProp.Plots import Prho
-        Prho(fluid_ref, show=True)
+        Prho(fluid_ref, show=False)
 
         from matplotlib import pyplot
         fig = pyplot.figure(2)
         ax = fig.gca()
-        Prho(fluid_ref, show=True, axis=ax)
-
-        Prho(fluid_ref, show=True, Tmin=200, Tmax=300)
+        Prho(fluid_ref, show=False, axis=ax)
+        plt.close()
+        Prho(fluid_ref, show=False, Tmin=200, Tmax=300)
+        plt.close()
 
     def Trho_plot_tests():
         from CoolProp.Plots import Trho
-        Trho(fluid_ref, show=True)
+        Trho(fluid_ref, show=False)
 
         from matplotlib import pyplot
         fig = pyplot.figure(2)
         ax = fig.gca()
-        Trho(fluid_ref, show=True, axis=ax)
-
-        Trho(fluid_ref, show=True, Tmin=200, Tmax=300)
-
+        Trho(fluid_ref, show=False, axis=ax)
+        plt.close()
+        Trho(fluid_ref, show=False, Tmin=200, Tmax=300)
+        plt.close()
+        
     def hs_plot_tests():
         from CoolProp.Plots import hs
-        hs(fluid_ref, show=True)
+        hs(fluid_ref, show=False)
 
         from matplotlib import pyplot
         fig = pyplot.figure(2)
         ax = fig.gca()
-        hs(fluid_ref, show=True, axis=ax)
-
-        hs(fluid_ref, show=True, Tmin=200, Tmax=300)
-
+        hs(fluid_ref, show=False, axis=ax)
+        plt.close()
+        hs(fluid_ref, show=False, Tmin=200, Tmax=300)
+        plt.close()
+        
     def Isolines_plot_tests():
         from matplotlib import pyplot
         from CoolProp.Plots import Ts, drawIsoLines
@@ -136,7 +97,7 @@ def test_back_compatibility():
         quality = drawIsoLines(fluid_ref, 'Ts', 'Q', [0.3, 0.5, 0.7, 0.8], axis=ax)
         isobars = drawIsoLines(fluid_ref, 'Ts', 'P', [100, 2000], num=5, axis=ax)
         isochores = drawIsoLines(fluid_ref, 'Ts', 'D', [2, 600], num=7, axis=ax)
-        pyplot.show()
+        pyplot.close()
 
     Ts_plot_tests()
     Ph_plot_tests()
@@ -153,49 +114,47 @@ def test_new_code():
 
     def Ts_plot_tests():
         from CoolProp.Plots import PropsPlot
-        plt = PropsPlot(fluid_ref, 'Ts')
-        plt.show()
+        PP = PropsPlot(fluid_ref, 'Ts')
+        plt.close()
 
     def Ph_plot_tests():
         from CoolProp.Plots import PropsPlot
-        plt = PropsPlot(fluid_ref, 'Ph')
-        plt.show()
+        PP = PropsPlot(fluid_ref, 'Ph')
+        plt.close()
 
     def Isolines_plot_tests():
         from CoolProp.Plots import PropsPlot
-        plt = PropsPlot(fluid_ref, 'Ts')
+        PP = PropsPlot(fluid_ref, 'Ts')
         #plt.set_axis_limits([-0.5, 1.5, 300, 530])
-        plt.draw_isolines('Q', [0.3, 0.5, 0.7, 0.8])
-        plt.draw_isolines('P', [100, 2000], num=5)
-        plt.draw_isolines('D', [2, 600], num=7)
-        plt.show()
+        PP.draw_isolines('Q', [0.3, 0.5, 0.7, 0.8])
+        PP.draw_isolines('P', [100, 2000], num=5)
+        PP.draw_isolines('D', [2, 600], num=7)
+        plt.close()
 
     def Graph_annotations():
         from CoolProp.Plots import PropsPlot, IsoLines
-        plt = PropsPlot(fluid_ref, 'Ts')
-        plt.draw_isolines('Q', [0.3, 0.5, 0.7, 0.8])
-        plt.draw_isolines('P', [100, 2000], num=5)
-        plt.draw_isolines('D', [2, 600], num=7)
+        PP = PropsPlot(fluid_ref, 'Ts')
+        PP.draw_isolines('Q', [0.3, 0.5, 0.7, 0.8])
+        PP.draw_isolines('P', [100, 2000], num=5)
+        PP.draw_isolines('D', [2, 600], num=7)
         plt.title('New Title')
-        plt.xlabel('New x label')
-        plt.ylabel('New y label')
-        plt.show()
-        plt = IsoLines(fluid_ref, 'Ts', 'P')
-        plt.draw_isolines([100, 2000], num=5)
-        plt.show()
+        PP.xlabel('New x label')
+        PP.ylabel('New y label')
+        PP = IsoLines(fluid_ref, 'Ts', 'P')
+        PP.draw_isolines([100, 2000], num=5)
+        plt.close()
 
     def Mixture():
         from CoolProp.Plots import PropsPlot
-        plt = PropsPlot('REFPROP-MIX:R32[0.47319469]&R125[0.2051091]&R134a[0.32169621]', 'TD')
-        plt._plot_default_annotations()
-        plt.show()
+        PP = PropsPlot('REFPROP-MIX:R32[0.47319469]&R125[0.2051091]&R134a[0.32169621]', 'TD')
+        PP._plot_default_annotations()
+        plt.close()
 
     Ts_plot_tests()
     Ph_plot_tests()
     Isolines_plot_tests()
     Graph_annotations()
     Mixture()
-
 
 if __name__=='__main__':
     import nose
