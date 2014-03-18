f51ed3ee6fe039fd71ec114395dc8376b4277847
jowr <jowr@mek.dtu.dk>
2014-03-18 14:05:10 +0100

    Removed Prandl number

--- wrappers/Modelica/src/coolpropsolver.cpp
+++ wrappers/Modelica/src/coolpropsolver.cpp
@@ -222,11 +222,9 @@ void CoolPropSolver::postStateChange(ExternalThermodynamicState *const propertie
 				{
 					properties->eta = state->viscosity();
 					properties->lambda = state->conductivity(); //[kW/m/K --> W/m/K]
-					properties->Pr = properties->cp*properties->eta/properties->lambda;
 				} else {
 					properties->eta    = -_HUGE;
 					properties->lambda = -_HUGE;
-					properties->Pr     = -_HUGE;
 				}
 			}
 			catch(std::exception &e)
@@ -255,11 +253,9 @@ void CoolPropSolver::postStateChange(ExternalThermodynamicState *const propertie
 				{
 					properties->eta = state->viscosity();
 					properties->lambda = state->conductivity(); //[kW/m/K --> W/m/K]
-					properties->Pr = properties->cp*properties->eta/properties->lambda;
 				} else {
 					properties->eta    = -_HUGE;
 					properties->lambda = -_HUGE;
-					properties->Pr     = -_HUGE;
 				}
 			}
 			catch(std::exception &e)
