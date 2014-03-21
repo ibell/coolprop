da5bcebeb7f5580d47d27d80de5aeddf2ff31387
Ian Bell <ian.h.bell@gmail.com>
2014-03-21 15:10:30 +0100

    Switched EES wrapper over to use InnoSetup installer rather than annoying Visual Studio MSI builder. EES doesn't use the DLL anymore - can debug the DLF directly, instructions added to README.rst
    Signed-off-by: Ian Bell <ian.h.bell@gmail.com>

--- wrappers/EES/BuildDLF.bat
+++ wrappers/EES/BuildDLF.bat
@@ -2,8 +2,11 @@ REM ******** set the variables ************
 call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 call "C:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 
-REM ******* compile all the sources from CoolProp***************
-cl /c /EHsc /I../../CoolProp maindll.cpp ../../CoolProp/CoolPropTools.cpp
-link /DLL maindll.obj CoolPropTools.obj /OUT:COOLPROP_EES.dlf
+REM ******* compile all the sources ***************
+cl /c /MP3 /I../../CoolProp /EHsc ../../CoolProp/*.cpp
+
+REM ******* compile the wrapper ***************
+cl /c /EHsc /I../../CoolProp main.cpp
+link /DLL *.obj /OUT:COOLPROP_EES.dlf
 
 erase *.obj
