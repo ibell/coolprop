bcaae4775085d3ebabe428321a3e9e481e8a3f18
jowr <jowr@mek.dtu.dk>
2014-07-09 17:40:12 +0200

    Added debug output for Modelica

--- wrappers/Modelica/src/coolpropsolver.cpp
+++ wrappers/Modelica/src/coolpropsolver.cpp
@@ -268,6 +268,12 @@ void CoolPropSolver::postStateChange(ExternalThermodynamicState *const propertie
 			errorMessage((char*)"Invalid fluid type!");
 			break;
 	}
+	if (debug_level > 50) std::cout << format("At the end of %s \n","postStateChange");
+	if (debug_level > 50) std::cout << format("Setting pressure to %f \n",properties->p);
+	if (debug_level > 50) std::cout << format("Setting temperature to %f \n",properties->T);
+	if (debug_level > 50) std::cout << format("Setting density to %f \n",properties->d);
+	if (debug_level > 50) std::cout << format("Setting enthalpy to %f \n",properties->h);
+	if (debug_level > 50) std::cout << format("Setting entropy to %f \n",properties->s);
 }
 
 
